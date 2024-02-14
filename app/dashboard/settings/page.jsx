8ffab7cc16cfa10c9ee00878f73cd9c5a33034b6
badiskefi <badiskefi@gmail.com>
2024-02-14 22:42:55 +0100

    useRouter redirect

--- app/dashboard/settings/page.jsx
+++ app/dashboard/settings/page.jsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 export const metadata = {
   title: "settings page title",
 };
 export default function Page() {
+  /* redirect("https://www.google.com");
+  redirect("/login"); */
   return (
     <>
       <Link href={"/dashboard"}>go to dashboard from settings page</Link>
+      <Link href={"dashboard/extra"}>
+        test leaf replacement from settings page
+      </Link>
       <div>dashboard settings page</div>
       <div>.</div>
       <div>.</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Home() {
   const pathname = usePathname();
+  const router = useRouter();
   console.log("pathname : " + pathname);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+      <button onClick={() => router.push("/dashboard")}>Dashboard</button>
       {[...Array(4)].map((_, index) => (
         <Link
           className={buttonVariants({ variant: "link" })}
